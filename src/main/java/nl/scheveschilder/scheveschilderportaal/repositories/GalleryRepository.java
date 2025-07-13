3f254137edafaba972f1f3bd003a1eb934905f68
Hans Bregman <hns101@live.nl>
2025-07-13 19:44:40 +0200

    updated gallery Respo

--- src/main/java/nl/scheveschilder/scheveschilderportaal/repositories/GalleryRepository.java
+++ src/main/java/nl/scheveschilder/scheveschilderportaal/repositories/GalleryRepository.java
@@ -4,8 +4,12 @@ import nl.scheveschilder.scheveschilderportaal.models.Gallery;
 import nl.scheveschilder.scheveschilderportaal.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GalleryRepository extends JpaRepository<Gallery, Long> {
     Optional<Gallery> findByStudent(Student student);
+
+    // This will find all galleries where the 'isPublic' field is true.
+    List<Gallery> findAllByIsPublic(boolean isPublic);
 }
